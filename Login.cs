de81c6b94fc772465f1cde31e0c3d70442310af4
Thobani Ndlela <thobani.ndlela@dutappfactory.co.za>
2023-11-06 13:43:30 +0200

    Login code uncommented

--- Login.cs
+++ Login.cs
@@ -20,43 +20,37 @@ namespace NorthwindSupplierManagementSystem
             string username = txt_WorkEmail.Text;
             string password = txt_Password.Text;
 
+            if (!_managerLogic.IsValidEmail(username))
+            {
+                MessageBox.Show("Invalid email.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (!_managerLogic.IsValidPassword(password))
+            {
+                MessageBox.Show("Invalid password. Requires a minimum length of 8 characters, at least one uppercase letter, one lowercase letter, one digit, and one special character.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var manager = _managerLogic.GetManagerByEmailAndPassword(username, password);
 
-            DashBoard dashBoard = new DashBoard(this);
-            dashBoard.Show();
-            txt_Password.Clear();
-            this.Hide();
-
-            //if(!_managerLogic.IsValidEmail(username))
-            //{
-            //    MessageBox.Show("Invalid email.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //}
-            //else if (!_managerLogic.IsValidPassword(password))
-            //{
-            //    MessageBox.Show("Invalid password. Requires a minimum length of 8 characters, at least one uppercase letter, one lowercase letter, one digit, and one special character.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //}
-            //else
-            //{
-            //    var manager = _managerLogic.GetManagerByEmailAndPassword(username, password);
-
-            //    if (manager is not null)
-            //    {
-            //        if(manager.WorkEmail is null || manager.WorkEmail == string.Empty) 
-            //        {
-            //            MessageBox.Show("User Not Found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //        }
-            //        else 
-            //        {
-            //            DashBoard dashBoard = new DashBoard(this);
-            //            dashBoard.Show();
-            //            txt_Password.Clear();
-            //            this.Hide();
-            //        }
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show("Login failed. Please check your credentials.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //}
+                if (manager is not null)
+                {
+                    if (manager.WorkEmail is null || manager.WorkEmail == string.Empty)
+                    {
+                        MessageBox.Show("User Not Found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        DashBoard dashBoard = new DashBoard(this);
+                        dashBoard.Show();
+                        txt_Password.Clear();
+                        this.Hide();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Login failed. Please check your credentials.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
     }
